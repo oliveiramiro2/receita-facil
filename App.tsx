58c66987f2fea95bc7c841e551e4e27a2309eca3
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-04 03:36:17 -0300

    called the routes into app
    create and config project

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+import RoutesReceitaFacil from './src/routes';
 
 const App: React.FC = () => (
-    <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+        <RoutesReceitaFacil />
+    </NavigationContainer>
 );
 
 export default App;
