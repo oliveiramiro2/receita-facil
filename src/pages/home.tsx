b07e7394f3ae7f9ca3098e29f842f5004fb9c630
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-04 04:26:43 -0300

    added layout of input
    finished today (first day)
    
    add home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,5 +1,14 @@
-import { Text, StyleSheet, SafeAreaView } from 'react-native';
-import React from 'react';
+import {
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    View,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
+
 import { Logo } from '../components';
 
 const styles = StyleSheet.create({
@@ -9,11 +18,49 @@ const styles = StyleSheet.create({
         paddingTop: 36,
         paddingHorizontal: 14,
     },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#0e0e0e',
+    },
+    form: {
+        width: '100%',
+        borderRadius: 8,
+        marginVertical: 16,
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: '#ecece',
+        paddingHorizontal: 8,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    input: {
+        width: '90%',
+        maxWidth: '90%',
+        height: 54,
+    },
 });
 
-export const Home: React.FC = () => (
-    <SafeAreaView style={[styles.contain]}>
-        <Logo />
-        <Text>home</Text>
-    </SafeAreaView>
-);
+export const Home: React.FC = () => {
+    const [search, setSearch] = useState<string>('');
+
+    return (
+        <SafeAreaView style={[styles.contain]}>
+            <Logo />
+            <Text style={[styles.title]}>Encontre a receita</Text>
+            <Text style={[styles.title]}>que combina com você</Text>
+            <View style={[styles.form]}>
+                <TextInput
+                    placeholder="Digite o nome de uma comida..."
+                    style={[styles.input]}
+                    value={search}
+                    onChangeText={setSearch}
+                />
+                <TouchableOpacity>
+                    <Ionicons name="search" size={28} color="#4cbe6c" />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+};
