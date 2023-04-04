7071b79f223da6dbf8762ac0eac0467e995b1c9f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-04 04:06:40 -0300

    added component logo layout
    add logo components

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import React from 'react';
 
+const styles = StyleSheet.create({
+    contain: {
+        backgroundColor: '#4cbe6c',
+        alignSelf: 'flex-start',
+        paddingVertical: 8,
+        paddingLeft: 16,
+        paddingRight: 20,
+        borderTopRightRadius: 8,
+        borderBottomLeftRadius: 8,
+        borderTopLeftRadius: 8,
+        borderBottomRightRadius: 32,
+        marginBottom: 8,
+    },
+
+    logoText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#fff',
+    }
+});
+
 export const Logo = () => (
-    <View>
-        <Text>logo</Text>
+    <View style={[styles.contain]}>
+        <Text>Receita Fácil</Text>
     </View>
 );
