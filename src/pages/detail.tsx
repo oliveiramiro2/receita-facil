d0098286fae67ff1f419f1a6fddc156b4446b323
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-04 15:24:17 -0300

    added heart into header of datail page
    finished today (second day)
    
    add detail

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -1,7 +1,11 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
-import { useRoute, RouteProp } from '@react-navigation/native';
+import { View, Text, StyleSheet, Pressable } from 'react-native';
+import React, { useLayoutEffect } from 'react';
+import { useRoute, useNavigation, RouteProp } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Entypo } from '@expo/vector-icons';
+
 import { IDataListFood } from '../services/data';
+import { stacks } from '../routes/stack';
 
 type ParamList = {
     Detail: {
@@ -17,6 +21,20 @@ const styles = StyleSheet.create({
 
 export const Detail: React.FC = () => {
     const route = useRoute<RouteProp<ParamList, 'Detail'>>();
+    const navigation = useNavigation<NativeStackNavigationProp<stacks>>();
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: route.params?.data
+                ? route.params?.data.name
+                : 'Detalhes da receita',
+            headerRight: () => (
+                <Pressable>
+                    <Entypo name="heart" size={28} color="#ff4141" />
+                </Pressable>
+            ),
+        });
+    }, [navigation, route.params?.data]);
 
     return (
         <View style={[styles.contain]}>
