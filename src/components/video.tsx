9bf7e5fcc5941c1d30a3f201d442858a776650ef
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-05 19:50:33 -0300

    added a simple webview
    add video component

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,12 +1,12 @@
 import {
     StyleSheet,
     Text,
-    View,
     TouchableOpacity,
     SafeAreaView,
 } from 'react-native';
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
+import WebView from 'react-native-webview';
 
 interface IPropsVideo {
     handleClose: Function;
@@ -32,6 +32,10 @@ const styles = StyleSheet.create({
         fontWeight: '500',
         marginLeft: 14,
     },
+    contentView: {
+        flex: 1,
+        width: '100%',
+    },
 });
 
 export const Video: React.FC<IPropsVideo> = ({ handleClose, link }) => (
@@ -40,6 +44,6 @@ export const Video: React.FC<IPropsVideo> = ({ handleClose, link }) => (
             <Feather name="arrow-left" size={24} color="#fff" />
             <Text style={[styles.backText]}>Voltar</Text>
         </TouchableOpacity>
-        <Text>video</Text>
+        <WebView source={{ uri: link }} style={[styles.contentView]} />
     </SafeAreaView>
 );
