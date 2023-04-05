567823b081031aec1b6abd7976a838c56cb7cb3f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-05 18:44:09 -0300

    adjust to make it better
    adjust detail page

--- src/pages/detail.tsx
+++ src/pages/detail.tsx
@@ -38,6 +38,7 @@ const styles = StyleSheet.create({
         top: 0,
         left: 0,
         right: 0,
+        bottom: 0,
         alignItems: 'center',
         justifyContent: 'center',
     },
