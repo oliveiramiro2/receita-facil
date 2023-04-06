a2e457028a761a8158a610cc828b79abe280dc5a
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-05 22:58:22 -0300

    added layout of page
    add search page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -9,9 +9,12 @@ import {
 } from 'react-native';
 import React, { useState } from 'react';
 import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { FoodList, Logo } from '../components';
 import { data } from '../services/data';
+import { stacks } from '../routes/stack';
 
 const styles = StyleSheet.create({
     contain: {
@@ -47,6 +50,16 @@ const styles = StyleSheet.create({
 export const Home: React.FC = () => {
     const [search, setSearch] = useState<string>('');
 
+    const navigation = useNavigation<NativeStackNavigationProp<stacks>>();
+
+    const handleSearch = (): undefined | void => {
+        if (search === '') return;
+
+        const input = search;
+        setSearch('');
+        navigation.navigate('Search', { name: input });
+    };
+
     return (
         <SafeAreaView style={[styles.contain]}>
             <Logo />
@@ -59,7 +72,7 @@ export const Home: React.FC = () => {
                     value={search}
                     onChangeText={setSearch}
                 />
-                <TouchableOpacity>
+                <TouchableOpacity onPress={handleSearch}>
                     <Ionicons name="search" size={28} color="#4cbe6c" />
                 </TouchableOpacity>
             </View>
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,14 +1,56 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
+
+import { data, IDataListFood } from '../services/data';
+import { FoodList } from '../components';
+
+type ParamList = {
+    Search: {
+        name: string;
+    };
+};
 
 const styles = StyleSheet.create({
     contain: {
         flex: 1,
+        paddingHorizontal: 14,
+        backgroundColor: '#f3f9ff',
+        paddingTop: 14,
+    },
+    textError: {
+        fontSize: 16,
+        fontWeight: '300',
     },
 });
 
-export const Search: React.FC = () => (
-    <View style={[styles.contain]}>
-        <Text>search</Text>
-    </View>
-);
+export const Search: React.FC = () => {
+    const [filted, setFilted] = useState<IDataListFood[]>([]);
+    const route = useRoute<RouteProp<ParamList, 'Search'>>();
+
+    useEffect(() => {
+        const foodFounded: IDataListFood[] = [];
+        data.forEach((item) => {
+            if (item.name.includes(route.params?.name)) {
+                foodFounded.push(item);
+            }
+        });
+        setFilted(foodFounded);
+    }, [route.params?.name]);
+
+    return (
+        <View style={[styles.contain]}>
+            <FlatList
+                showsVerticalScrollIndicator
+                data={filted}
+                keyExtractor={(item) => String(item.id)}
+                renderItem={({ item }) => <FoodList data={item} />}
+                ListEmptyComponent={() => (
+                    <Text style={[styles.textError]}>
+                        Não foi possível encontrar a receita...
+                    </Text>
+                )}
+            />
+        </View>
+    );
+};
