8101e088945faad3e9ab0ae2581f1aa465828dd6
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-06 14:23:00 -0300

    added toas provider over the all aplication
    create and config project

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
+import { ToastProvider } from 'react-native-toast-notifications';
 
 import RoutesReceitaFacil from './src/routes';
 
 const App: React.FC = () => (
-    <NavigationContainer>
-        <RoutesReceitaFacil />
-    </NavigationContainer>
+    <ToastProvider>
+        <NavigationContainer>
+            <RoutesReceitaFacil />
+        </NavigationContainer>
+    </ToastProvider>
 );
 
 export default App;
