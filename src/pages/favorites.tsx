a296441e1d855901a39c52777eb9267106a9dc62
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-05 22:28:10 -0300

    added layout of page
    add favorites page

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,14 +1,63 @@
-import { View, Text, StyleSheet } from 'react-native';
-import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { useIsFocused } from '@react-navigation/native';
+
+import { IDataListFood } from '../services/data';
+import { getFavorite } from '../utils/async';
+import { FoodList } from '../components';
 
 const styles = StyleSheet.create({
     contain: {
         flex: 1,
+        backgroundColor: '#f3f9ff',
+        paddingHorizontal: 14,
+        paddingTop: 36,
+    },
+    title: {
+        color: '#000',
+        fontWeight: 'bold',
+        fontSize: 24,
     },
 });
 
-export const Favorites: React.FC = () => (
-    <View style={[styles.contain]}>
-        <Text>favorites</Text>
-    </View>
-);
+export const Favorites: React.FC = () => {
+    const [receive, setReceive] = useState<IDataListFood[]>([]);
+
+    const isFocused = useIsFocused();
+
+    useEffect(() => {
+        let isActive = true;
+
+        const getReceive = async () => {
+            const result = await getFavorite('@appreceitas');
+            if (result !== false && isActive) {
+                setReceive(result);
+            }
+        };
+
+        if (isActive) getReceive();
+
+        return () => {
+            isActive = false;
+        };
+    }, [isFocused]);
+
+    return (
+        <SafeAreaView style={[styles.contain]}>
+            <Text style={[styles.title]}>Receitas favoritas</Text>
+            {receive.length === 0 ? (
+                <View>
+                    <Text>Você ainda não tem nenhuma receita salva.</Text>
+                </View>
+            ) : (
+                <FlatList
+                    showsVerticalScrollIndicator
+                    data={receive}
+                    keyExtractor={(item) => String(item.id)}
+                    renderItem={({ item }) => <FoodList data={item} />}
+                    style={{ marginTop: 14 }}
+                />
+            )}
+        </SafeAreaView>
+    );
+};
